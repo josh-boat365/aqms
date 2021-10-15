301efae712c92422ff4eea762e06c047c7647b8a
Joshua Nyarko Boateng <nyarko.boateng20@outlook.com>
2021-10-15 04:51:27 +0000

    changed project structure

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+   
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./app/bootstrap/css/bootstrap.min.css">
+     <!-- Local - CSS -->
+    <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+    <!-- AOS Library -->
+    <link rel="stylesheet" href="./assets/css/aos.css">
+
+    <title>ATU - Alumni Questionnaire Tracer</title>
+  </head>
+  <body>
+    <div class="container">
+      <img src="./assets/images/atulogo.png" alt="atu-logo.png">
+      <div class="asset-1">
+        <img src="./assets/images/asst2.png" alt="Blob-2">
+        <img src="./assets/images/asst1.png" alt="Blob-1">
+      </div>
+      <div class="card">
+        <div class="card-body">
+          <h2 class="card-title text-center text-wrap">Welcome to Accra Technical University <br> Tracer Study</h2>
+          
+        </div>
+      </div>
+
+
+      
+      
+
+    </div>
+
+   
+
+
+
+   
+    <!-- JavaScript -->
+    <!--  Bootstrap JS -->
+    <script src="./app/bootstrap/js/bootstrap.min.js"></script>
+    <!-- AOS Library -->
+    <script src="./assets/js/aos.js"></script>
+    <script src="./assets/js/aos-init.js"></script>
+  </body>
+</html>
