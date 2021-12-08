c0d5a0e2554abd2e4f69131e99879d4bc7ad488f
Joshua Nyarko Boateng <60707857+josh-boat365@users.noreply.github.com>
2021-12-08 17:58:57 +0000

    #16 feature update on question card

--- app/resources/views/inc/alumnus/question/question-card.blade.php
+++ app/resources/views/inc/alumnus/question/question-card.blade.php
@@ -1,69 +1,74 @@
+{{-- style for question cards  --}}
+
+<style type="text/css">
+    .card-style{
+        width: 65% !important;
+        margin: 0 auto;
+        }
+</style>
+
 @for ($i = 0; $i < count($survey->questions); $i++)
 
-    <div class="@if ($survey->questions[$i]->option_type_id == 5) col @else col-8 @endif">
-        <div class="card question d-flex mb-4 edit-quesiton">
+    <div class="@if ($survey->questions[$i]->option_type_id == 5) col-12 @else col-lg-12 @endif">
+        <div class="card question d-flex mb-4 card-style">
             <div class="d-flex flex-grow-1 min-width-zero">
-                <div
-                    class="card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
-                    <div class="list-item-heading mb-0 truncate w-80 mb-1 mt-1"><span
-                            class="heading-number d-inline-block">{{ $i + 1 }}
-                        </span></div>
-                </div>
+                <div class="card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
+                    <div class="list-item-heading mb-0  w-80 d-flex mb-1 mt-1">
 
-            </div>
-            <div class="question-collapse collapse show" id="q{{ $survey->questions[$i]->id }}">
-                <div class="card-body pt-0">
-                    {{-- view --}}
-                    <div class="edit-mode">
-                        <label class="preview-question">
+                        {{-- question-number --}}
+                        <div class="heading-number d-inline-block"> {{ $i + 1 }} </div>
+
+                        {{-- the question itself --}}
+                        <div class="ques-header" id="q{{ $survey->questions[$i]->id }}">
                             {{ $survey->questions[$i]->question }}
-                        </label>
-                        {{-- 1 - text (single line)
-                            4 - text (multi-line)
-                                                2 - radio
-                                                3 - check
-                                                5 - grid --}}
-                        <div class="mb-4">
-                            @if ($survey->questions[$i]->option_type_id == 1)
-                                <input class="form-control" type="text" name="ans[{{ $survey->questions[$i]->id }}]"
-                                    @foreach ($responses->where('question_id', $survey->questions[$i]->id)->get() as $response)
-                                value="{{ $response->response }}"
-                            @endforeach
-                            >
-                        @elseif ($survey->questions[$i]->option_type_id == 2)
-                            <textarea class="form-control" name="ans[{{ $survey->questions[$i]->id }}]" cols="30"
-                                rows="2">@foreach ($responses->where('question_id', $survey->questions[$i]->id)->get() as $response){{ $response->response }} @endforeach
-                                </textarea>
+                        </div>
 
-                        @elseif ($survey->questions[$i]->option_type_id == 3)
-                            @foreach ($survey->options->where('question_id', $survey->questions[$i]->id) as $option)
-                                <div class="custom-control custom-radio"><input value="{{ $option->option }}"
-                                        type="radio" id="o{{ $option->id }}"
-                                        name="ans[{{ $survey->questions[$i]->id }}]" class="custom-control-input"
-                                        {{-- @foreach ($responses->where('question_id', $survey->questions[$i]->id)->get() as $response)
-                                            @if ($response->response == $option->option)
-                                                checked
-                                            @endif
-                                        @endforeach --}}>
-                                    <label class="custom-control-label"
-                                        for="o{{ $option->id }}">{{ $option->option }}</label>
-                                </div>
-                            @endforeach
-
-
-                        @elseif ($survey->questions[$i]->option_type_id == 4)
-                            @foreach ($survey->options->where('question_id', $survey->questions[$i]->id) as $option)
-                                <div class="custom-control custom-checkbox">
-                                    <input value="{{ $option->option }}" class="custom-control-input" type="checkbox"
-                                        name="ans[{{ $survey->questions[$i]->id }}][]" id="o{{ $option->id }}"
-                                        @foreach ($responses->where('question_id', $survey->questions[$i]->id)->get() as $response)
+                    </div>
+                </div>
+            </div>
+            {{-- question options  --}}
+            <div class="card-body ">
+                <div class="mb-4">
+                    @if ($survey->questions[$i]->option_type_id == 1)
+                        <input class="form-control" type="text" name="ans[{{ $survey->questions[$i]->id }}]"
+                            @foreach ($responses->where('question_id', $survey->questions[$i]->id)->get() as $response)
+                        value="{{ $response->response }}"
+                    @endforeach>
+                @elseif ($survey->questions[$i]->option_type_id == 2)
+                    <textarea class="form-control" name="ans[{{ $survey->questions[$i]->id }}]" cols="30"
+                        rows="2">@foreach ($responses->where('question_id', $survey->questions[$i]->id)->get() as $response){{ $response->response }} @endforeach
+                        </textarea>
+    
+                @elseif ($survey->questions[$i]->option_type_id == 3)
+                    @foreach ($survey->options->where('question_id', $survey->questions[$i]->id) as $option)
+                        <div class="custom-control custom-radio"><input value="{{ $option->option }}"
+                                type="radio" id="o{{ $option->id }}"
+                                name="ans[{{ $survey->questions[$i]->id }}]" class="custom-control-input"
+                                {{-- @foreach ($responses->where('question_id', $survey->questions[$i]->id)->get() as $response)
                                     @if ($response->response == $option->option)
                                         checked
                                     @endif
-                            @endforeach>
-                            <label for="o{{ $option->id }}"
-                                class="custom-control-label">{{ $option->option }}</label>
+                                @endforeach --}}>
+                            <label class="custom-control-label"
+                                for="o{{ $option->id }}">{{ $option->option }}</label>
                         </div>
+                    @endforeach
+    
+    
+                @elseif ($survey->questions[$i]->option_type_id == 4)
+                    @foreach ($survey->options->where('question_id', $survey->questions[$i]->id) as $option)
+                        <div class="custom-control custom-checkbox">
+                            <input value="{{ $option->option }}" class="custom-control-input" type="checkbox"
+                                name="ans[{{ $survey->questions[$i]->id }}][]" id="o{{ $option->id }}"
+                                @foreach ($responses->where('question_id', $survey->questions[$i]->id)->get() as $response)
+                            @if ($response->response == $option->option)
+                                checked
+                            @endif
+                    @endforeach>
+                    <label for="o{{ $option->id }}"
+                        class="custom-control-label">{{ $option->option }}</label>
+                </div>
+            </div>
 
 @endforeach
 
