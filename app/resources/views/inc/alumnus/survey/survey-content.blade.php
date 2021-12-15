105f94e6f047cb79e46c7b9c836e40f13116fc4f
Joshua Nyarko Boateng <60707857+josh-boat365@users.noreply.github.com>
2021-12-15 18:30:41 +0000

    made survey tiles responsive on alumi home

--- app/resources/views/inc/alumnus/survey/survey-content.blade.php
+++ app/resources/views/inc/alumnus/survey/survey-content.blade.php
@@ -1,62 +1,20 @@
+<style>
+    .tile-header h5{
+        width: 142%;
+        padding: 0;
+        margin: 0;
+    }
+</style>
 <main>
     <div class="container-fluid">
         <div class="row app-row">
             <div class="col-12">
                 <div class="mb-2">
                     <h1>Surveys</h1>
-                    
-                    <div class="modal fade modal-right" id="exampleModal" tabindex="-1" role="dialog"
-                        aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Add New</h5><button
-                                        type="button" class="close" data-dismiss="modal"
-                                        aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                </div>
-                                <div class="modal-body">
-                                    <form>
-                                        <div class="form-group"><label>Title</label> <input type="text"
-                                                class="form-control" placeholder=""></div>
-                                        <div class="form-group"><label>Details</label> <textarea
-                                                class="form-control" rows="2"></textarea></div>
-                                        <div class="form-group"><label>Category</label> <select
-                                                class="form-control select2-single" data-width="100%">
-                                                <option label="&nbsp;">&nbsp;</option>
-                                                <option value="Flexbox">Flexbox</option>
-                                                <option value="Sass">Sass</option>
-                                                <option value="React">React</option>
-                                            </select></div>
-                                        <div class="form-group"><label>Labels</label> <select
-                                                class="form-control select2-multiple" multiple="multiple"
-                                                data-width="100%">
-                                                <option value="New Framework">New Framework</option>
-                                                <option value="Education">Education</option>
-                                                <option value="Personal">Personal</option>
-                                            </select></div>
-                                        <div class="form-group"><label>Status</label>
-                                            <div class="custom-control custom-checkbox"><input type="checkbox"
-                                                    class="custom-control-input" id="customCheck1"> <label
-                                                    class="custom-control-label"
-                                                    for="customCheck1">Completed</label>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                                <div class="modal-footer"><button type="button" class="btn btn-outline-primary"
-                                        data-dismiss="modal">Cancel</button> <button type="button"
-                                        class="btn btn-primary">Submit</button></div>
-                            </div>
-                        </div>
-                    </div>
                 </div>
-                <div class="mb-2"><a class="btn pt-0 pl-0 d-inline-block d-md-none" data-toggle="collapse"
-                        href="#displayOptions" role="button" aria-expanded="true"
-                        aria-controls="displayOptions">Display
-                        Options <i class="simple-icon-arrow-down align-middle"></i></a>
-                   
-                </div>
+               
                 <div class="separator mb-5"></div>
+                
                 @include('welcome')
                   {{-- Survey Tile Header  --}}
                   <div class="card mb-5 tile-header">
--- app/resources/views/inc/alumnus/survey/survey-tile.blade.php
+++ app/resources/views/inc/alumnus/survey/survey-tile.blade.php
@@ -11,12 +11,25 @@
     @media (min-width: 320px) and (max-width: 656px) {
         .dash-survey-tile {
             display: flex !important;
-            flex-direction: column !important;
+            flex-direction: row !important;
         }
+        .tile-text{
+            font-size: 74%;
+        }
+        .surv-name{
+            white-space: normal !important;
+            position: relative;
+            left: -1rem;
+        }
+        .surv-stats{
+            padding: 5px;
+            font-size: 8px;
 
+        }
+        
         .tile-info p {
-            width: 7rem !important;
-            margin: 5px !important;
+            width: 4rem !important;
+            
         }
 
         .tile-header .card-body .col-3 h5 {
@@ -32,19 +45,17 @@
 
 <div class="card d-flex flex-row mb-3">
     <div class="d-flex flex-grow-1 min-width-zero">
-        <div
-            class="dash-survey-tile card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
+        <div class="dash-survey-tile card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
             <div class="col-3 tile-info">
-                <h5 class="font-weight-bold h5">Title</h5>
                 <span class="list-item-heading mb-0 truncate w-40 w-xs-100 mt-0">
 
-                    <div class="align-middle d-inline-block">{{ $survey->name }}</div>
+                    <div class="align-middle surv-name tile-text d-inline-block">{{ $survey->name }}</div>
                 </span>
             </div>
 
             <div class="col-3 title-info">
                 <div class="w-15 w-xs-100">
-                    <p class="mb-0 text-muted font-weight-bold  text-medium">
+                    <p class="mb-0 text-muted tile-text font-weight-bold  text-medium">
                         {{-- {{$survey->questions->count()}} --}}
                         @if ($progresses->where('user_id', auth()->user()->id)->where('survey_id', $survey->id)->count() > 0)
                             @foreach ($progresses->where('user_id', auth()->user()->id)->where('survey_id', $survey->id) as $progress)
@@ -61,7 +72,7 @@
 
             <div class="col-3 tile-info">
                 <div class="w-8 w-xs-100">
-                    <p class="mb-0 tile-x text-muted font-weight-bold text-medium">
+                    <p class="mb-0 tile-x tile-text text-muted font-weight-bold text-medium">
                         {{ $survey->created_at->format('Y-m-d') }}</p>
                 </div>
             </div>
@@ -69,7 +80,7 @@
             <div class="col-3 tile-info">
                 <div class="w-8 w-xs-100">
                     <p
-                        class="mb-0 font-weight-bold text-medium badge badge-pill  @if ($submissions->where('user_id', auth()->user()->id)->where('survey_id', $survey->id)->count() > 0)
+                        class="mb-0 font-weight-bold surv-stats  text-medium badge badge-pill  @if ($submissions->where('user_id', auth()->user()->id)->where('survey_id', $survey->id)->count() > 0)
                             badge-success">
                         submitted
                     @else
