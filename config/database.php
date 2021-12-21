c4722a7c4e92e2fa821f6cb156f9eae758b40867
Joshua Nyarko Boateng <60707857+josh-boat365@users.noreply.github.com>
2021-12-21 21:55:24 +0000

    repo structure for hosting

--- config/database.php
+++ config/database.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Str;
 
+//database Url for pgsql
+$DATABASE_URL = parse_url('DATABASE_URL');
+
 return [
 
     /*
@@ -66,7 +69,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $DATABASE_URL('host'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
