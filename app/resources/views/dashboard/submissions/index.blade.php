46ec7f272af2a70c8e292679351d422fcf9c2e39
Joshua Nyarko Boateng <60707857+josh-boat365@users.noreply.github.com>
2021-12-21 04:09:09 +0000

    updates

--- app/resources/views/dashboard/submissions/index.blade.php
+++ app/resources/views/dashboard/submissions/index.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Admin Dashboard | ATU Tracer</title>
+    <title>Submissions | ATU Tracer</title>
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
     <link rel="stylesheet" href="{{ asset('font/iconsmind-s/css/iconsminds.css') }}">
     <link rel="stylesheet" href="{{ asset('font/simple-line-icons/css/simple-line-icons.css') }}">
