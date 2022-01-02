7eeb12e6c43b356de7188af48211aeee08974cdd
Joshua Nyarko Boateng <60707857+josh-boat365@users.noreply.github.com>
2022-01-02 05:28:10 +0000

    #61 added files for UI

---
+++ resources/views/inc/dashboard/responses/res-ques-analytics.blade.php
@@ -0,0 +1,20 @@
+<div class="card question d-flex mb-4 edit-quesiton">
+    <div class="d-flex flex-grow-1 min-width-zero">
+        <div class="card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
+            <div class="list-item-heading mb-0 truncate w-80 mb-1 mt-1">
+                <span class="heading-number d-inline-block">1 </span>Gender
+            </div>
+        </div>
+        <div class="custom-control custom-checkbox pl-1 align-self-center pr-4">
+            <button class="btn btn-outline-theme-3 icon-button">
+                <i class="iconsminds-files"></i>
+            </button> 
+        </div>
+    </div>
+    
+    <div class="card-body pt-0">
+        <h1>NO ANALYTICS YET</h1>
+        
+    </div>
+
+</div>
---
+++ resources/views/inc/dashboard/responses/res-ques-list.blade.php
@@ -0,0 +1,194 @@
+<div class="card mb-4 sticky-top" style="top: 115px; z-index: 0">
+    <div class="card-body">
+        <div class="d-flex">
+            <i class="iconsminds-repeat-2 h4"></i> &nbsp;
+            <p class="list-item-heading" style="position: relative; top:0.66rem">Textual Responses</p>
+        </div>
+        <div class="separator mb-3"></div>
+        <div>
+            <p class="list-item-heading" style="position: relative; top:0.66rem">Question 1</p>
+        </div>
+        <div class="scroll h-100  mt-2" style="max-height: 500px">
+            {{-- @foreach ($submissions as $submission) --}}
+                    {{-- <a href="#"> --}}
+                        <div class="alumni  alumnus-hover  ">
+                            {{-- @foreach ($users->where('id', $submission->user_id) as $user)
+                            <input type="hidden" name="user_id" value="{{$user->id}}">
+                                {{ $user->firstName }} {{ $user->lastName }}
+                            @endforeach --}}
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            
+                         
+                         
+                        </div>
+                    {{-- </a> --}}
+            {{-- @endforeach --}}
+          
+        </div>
+        <div class="separator mb-3"></div>
+        <div>
+            <p class="list-item-heading" style="position: relative; top:0.66rem">Question 2</p>
+        </div>
+        <div class="scroll h-100  mt-2" style="max-height: 500px">
+            {{-- @foreach ($submissions as $submission) --}}
+                    {{-- <a href="#"> --}}
+                        <div class="alumni  alumnus-hover ">
+                            {{-- @foreach ($users->where('id', $submission->user_id) as $user)
+                            <input type="hidden" name="user_id" value="{{$user->id}}">
+                                {{ $user->firstName }} {{ $user->lastName }}
+                            @endforeach --}}
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                            <p class="mb-0">The is Lord is Goood and all the Time God is good.</p>
+                            <div style="background-color:white; height: 3px"></div>
+                        </div>
+                    {{-- </a> --}}
+            {{-- @endforeach --}}
+          
+        </div>
+
+    </div>
+</div>
