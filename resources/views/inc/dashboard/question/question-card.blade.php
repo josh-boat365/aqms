dcc3c1ce45d2e7b91b519de5fbb5743e5419dad7
josh-boat365 <nyarko.boateng20@outlook.com>
2022-02-03 03:03:17 -0800

    question-card icons arrangements - admin

--- resources/views/inc/dashboard/question/question-card.blade.php
+++ resources/views/inc/dashboard/question/question-card.blade.php
@@ -9,7 +9,7 @@
                             class="heading-number d-inline-block">{{ $i + 1 }}
                         </span><span class="preview-question">{{ $survey->questions[$i]->question }}</span></div>
                 </div>
-                <div class="custom-control custom-checkbox pl-1 align-self-center pr-4" style="flex: 0 0 25%; max-width: 20%;">
+                <div class="custom-control d-flex custom-checkbox pl-1 align-self-center pr-4" style="flex: 0 0 25%; max-width: 20%;">
                     <div class="col btn btn-outline-theme-3 icon-button edit-button"><i class="simple-icon-pencil"></i></div>
                     <div class="col btn btn-outline-theme-3 icon-button view-button"><i class="simple-icon-eye"></i></div>
                     <div class="col btn btn-danger icon-button trash-button"><i class="simple-icon-trash"></i></div>
