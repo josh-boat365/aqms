1697150bd7d2f46db2a4fd933323ef0e7025dc47
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-04-01 04:24:22 -0500

    update

--- resources/views/inc/dashboard/question/question-card.blade.php
+++ resources/views/inc/dashboard/question/question-card.blade.php
@@ -1,7 +1,8 @@
 
 @for ($i = 0; $i < count($questions); $i++)
 
-        <div>
+        question
+        {{-- <div>
             <div class="card question d-flex mb-4 edit-quesiton">
                 <div class="d-flex flex-grow-1 min-width-zero">
                     <div
@@ -27,9 +28,9 @@
                 <div class="question-collapse collapse show" id="q{{ $questions[$i]['id'] }}">
                     <div class="card-body pt-0">
                         <div class="edit-mode">
-                            {{-- <input type="hidden"  ="ques[]" value="{{$i+1}}"> --}}
+
                             <div class="form-group mb-5 que-section"><label>Question</label>
-                                {{-- question --}}
+                                
                                 <input type="hidden" class="que-num" value="{{ $questions[$i]['id'] }}">
                                 <input class="form-control writtenQuestion" type="text"
                                     name="questions[old][{{ $questions[$i]['id'] }}]"
@@ -38,12 +39,12 @@
 
                             <div class="separator mb-4"></div>
 
-                            {{-- answer types select --}}
+                            
                             <div class="form-group opt-type"><label class="d-block">Answer Type</label>
-                                {{-- answer-type --}}
+                                
                                 <select class="form-control select2-single option-type" data-width="100%"
                                     name="questions[old][{{ $questions[$i]['id'] }}][option_type_id]">
-                                    {{-- <option label="&nbsp;">&nbsp;</option> --}}
+                                    
                                     @foreach ($optionTypes as $optionType)
                                         <option value="{{ $optionType->id }}" @if ($questions[$i]['option_type_id'] == $optionType->id) selected @endif>
                                             {{ $optionType->type }}
@@ -52,7 +53,7 @@
                                 </select>
                             </div>
 
-                            {{-- possible answers --}}
+                            
                             <div class="form-group ans-form">
                                 <div class="grid ans-group" 
                                 @if ($questions[$i]['option_type_id'] != 5) style="display: none" @endif>
@@ -66,9 +67,7 @@
                                                         <input class="form-control" type="text"
                                                             name="questions[old][{{ $questions[$i]['id'] }}][options][rows][old][{{ $option->id }}]"
                                                             value="{{ $option->option }}" id="{{$option->id}}">
-                                                        {{-- <input class="form-control" type="hidden" 
-                                                            name="ques[old][{{ $questions[$i]['id'] }}][options][rows][old][{{ $questions[$i]['id'] }}][id]"
-                                                            value="{{ $option->id }}"> --}}
+                                                            
                                                         <div class="input-icons"><span class="badge badge-pill handle pr-0 mr-0"><i
                                                                     class="simple-icon-cursor-move"></i> </span><span
                                                                 class="badge badge-pill btn del-ans"><i class="simple-icon-trash"></i></span></div>
@@ -83,9 +82,7 @@
                                                     <div class="mb-1 position-relative ans"><input class="form-control" type="text"
                                                             name="questions[old][{{ $questions[$i]['id'] }}][options][columns][old][{{ $option->id }}]"
                                                             value="{{ $option->option }}" id="{{ $option->id }}">
-                                                        {{-- <input class="form-control" type="hidden" 
-                                                            name="ques[old][{{ $questions[$i]['id'] }}][options][columns][old][{{ $questions[$i]['id'] }}][id]"
-                                                            value="{{ $option->id }}"> --}}
+                                                            
                                                         <div class="input-icons"><span class="badge badge-pill handle pr-0 mr-0"><i
                                                                     class="simple-icon-cursor-move"></i> </span><span
                                                                 class="badge badge-pill btn del-ans"><i class="simple-icon-trash"></i></span></div>
@@ -119,9 +116,7 @@
                                             <div class="mb-1 position-relative ans"><input class="form-control" type="text"
                                                     name="questions[old][{{ $questions[$i]['id'] }}][options][old][{{ $option->id }}]"
                                                     value="{{ $option->option }}" id="{{ $option->id }}">
-                                                {{-- <input class="form-control" type="hidden" 
-                                                    name="ques[old][{{ $questions[$i]['id'] }}][options][old][{{ $questions[$i]['id'] }}][id]"
-                                                    value="{{ $option->id }}"> --}}
+                                                    
                                                 <div class="input-icons"><span class="badge badge-pill handle pr-0 mr-0"><i
                                                             class="simple-icon-cursor-move"></i> </span><span
                                                         class="badge badge-pill btn del-ans"><i class="simple-icon-trash"></i></span></div>
@@ -137,76 +132,19 @@
                             </div>
                         </div>
 
-                        {{-- view --}}
+                        
                         <div class="view-mode">
                             <label class="preview-question">
-                                {{-- {{ $questions[$i]['question'] }} --}}
+                                
                             </label>
-                            {{-- 1 - text (single line)
-                                                    4 - text (multi-line)
-                                                                        2 - radio
-                                                                        3 - check
-                                                                        5 - grid --}}
+                            
                             <div class="mb-4">
-                                {{-- @if ($questions[$i]['option_type_id'] == 1)
-                                                        <input class="form-control" type="text" name="{{ $questions[$i]['id'] }}"
-                                                            id="{{ $questions[$i]['id'] }}">
-                                                    @elseif ($questions[$i]['option_type_id'] == 2)
-                                                    <textarea class="form-control" name="q{{$questions[$i]['id']}}" id="q{{$questions[$i]['id']}}" cols="30" rows="2"></textarea>
-
-                                                    @elseif ($questions[$i]['option_type_id'] == 3)
-                                                    @foreach ($survey->options->where('question_id', $questions[$i]['id']) as $option)
-                                                            <div class="custom-control custom-radio"><input type="radio" id="o{{ $option->id }}"
-                                                                    name="q{{ $questions[$i]['id'] }}" class="custom-control-input">
-                                                                <label class="custom-control-label" for="o{{ $option->id }}">{{ $option->option }}</label>
-                                                            </div>
-                                                        @endforeach
-                                                        
-
-                                                    @elseif ($questions[$i]['option_type_id'] == 4)
-                                                    @foreach ($survey->options->where('question_id', $questions[$i]['id']) as $option)
-                                                            <div class="custom-control custom-checkbox">
-                                                                <input class="custom-control-input" type="checkbox" name="" id="o{{ $option->id }}">
-                                                                <label for="o{{ $option->id }}" class="custom-control-label">{{ $option->option }}</label>
-                                                            </div>
-
-                                                        @endforeach
-
-
-                                                    @else
-                                                    <div class="row col-12">
-                                                        <div class="d-flex flex-column col-2">
-                                                            <div style="height: 50px"></div>
-                                                            @foreach ($survey->options->where('question_id', $questions[$i]['id']) as $option)
-                                                                <div class="text-center">{{ $option->option }}</div>
-                                                            @endforeach
-                                                        </div>
-                                                        <div class="col-10 row" style="flex-wrap: nowrap">
-                                                            @foreach ($survey->columns->where('question_id', $questions[$i]['id']) as $column)
-                                                                <div class="d-flex flex-column justify-content-between"
-                                                                    style="width: 100px; height: 100%; min-width:100px">
-                                                                    <div style="height: 50px"
-                                                                        class="d-flex align-items-center justify-content-center w-100">
-                                                                        {{ $column->question }}
-                                                                    </div>
-                                                                    @foreach ($survey->options->where('question_id', $questions[$i]['id']) as $option)
-                                                                        <div class="d-flex justify-content-center"><input type="radio"
-                                                                                name="ans[{{ $questions[$i]['id'] }}][{{ $option->id }}]"
-                                                                                id="" value="{{ $column->question }}"
-                                                                                
-                                                                                ></div>
-                                                                    @endforeach
-                                                                </div>
-                                                            @endforeach
-
-                                                        </div>
-                                                    </div>
-                                                    @endif --}}
+                                
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
     @endfor
