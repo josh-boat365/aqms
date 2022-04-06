9e12d0f325ba1ec2e70596086aa7faad1dae5776
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-04-06 00:49:09 -0500

    Update question-list.blade.php

--- resources/views/inc/dashboard/question/question-list.blade.php
+++ resources/views/inc/dashboard/question/question-list.blade.php
@@ -9,7 +9,7 @@
     @for ($i = 1; $i <= $survey->sections->count(); $i++)
         <div class="col-12 col-lg-8 survey-wrapper section-{{ $i }} @if ($i == 1) current @endif "
             style="display: none">
-            <input type="hidden" id="sec-num" value="{{ $i }}">
+            <input type="hidden" id="sec-num" value="{{ $survey->sections[$i - 1]->id }}">
             <div class=" mb-3  border-primary card section-card" style="border-top: 7px solid #f3f3f3">
                 <div class="position-absolute card-top-buttons">
                     <div class="btn icon-button btn-header-light sec-desc sec-edit"><i class="simple-icon-pencil"></i>
