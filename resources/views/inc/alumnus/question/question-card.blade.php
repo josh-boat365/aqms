e0a3b03eba7e3b3bebc959ebf31a6f64c173f524
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-04-06 05:40:09 -0500

    Update question-card.blade.php

--- resources/views/inc/alumnus/question/question-card.blade.php
+++ resources/views/inc/alumnus/question/question-card.blade.php
@@ -41,7 +41,7 @@
         <div class="d-flex flex-grow-1 min-width-zero">
             <div class="card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
                 <div class="list-item-heading mb-0 truncate w-80 mb-1 mt-1"><span class="heading-number d-inline-block">
-                    {{-- {{ ++$i }} --}}
+                    <!-- {{ ++$i }} -->
                     </span></div>
             </div>
         </div>
