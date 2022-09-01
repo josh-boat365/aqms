17cc70b1be25c0bdf72d408f21732ce429723f6d
josh-boat365 <nyarko.boateng20@outlook.com>
2022-09-01 15:21:03 +0000

    UI update on auth/register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
 @section('style')
-<link rel="stylesheet" href="{{asset('font/iconsmind-s/css/iconsminds.css')}}">
-<link rel="stylesheet" href="{{asset('font/simple-line-icons/css/simple-line-icons.css')}}">
-<link rel="stylesheet" href="{{asset('css/vendor/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/vendor/bootstrap.rtl.only.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/vendor/bootstrap-float-label.min.css')}}">
-<link rel="stylesheet" href="{{asset('css/main.css')}}">
-    
+    <link rel="stylesheet" href="{{ asset('font/iconsmind-s/css/iconsminds.css') }}">
+    <link rel="stylesheet" href="{{ asset('font/simple-line-icons/css/simple-line-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap.rtl.only.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/vendor/bootstrap-float-label.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 @endsection
 
-@section('body-class') class="background show-spinner no-footer" @endsection
+@section('body-class')
+    class="background show-spinner no-footer"
+@endsection
 
 @section('body')
     <div class="fixed-background"></div>
@@ -29,15 +30,20 @@
                             <p class="white">Thank you for the intended participation.</p>
 
                             <p class="text-wrap white">
-                                The Tracer Study seeks to learn about the extent to which the educational experience at Accra Technical University (ATU) has contributed to the career developments of its alumni.
+                                The Tracer Study seeks to learn about the extent to which the educational experience at
+                                Accra Technical University (ATU) has contributed to the career developments of its alumni.
                             </p>
 
                             <p class="text-wrap white">
-                                In particular, the study aims at determining, from your perspective, the impact of the training received on work placement and <span id="login-admin-btn">career</span> progression.
+                                In particular, the study aims at determining, from your perspective, the impact of the
+                                training received on work placement and <span id="login-admin-btn">career</span>
+                                progression.
                             </p>
 
                             <p class="text-wrap white">
-                                Your feedback, processed confidentially, will inform institutional policy on improving academic programmes and practical training, for quality service delivery to current students, prospective admissions, and industry.
+                                Your feedback, processed confidentially, will inform institutional policy on improving
+                                academic programmes and practical training, for quality service delivery to current
+                                students, prospective admissions, and industry.
                             </p>
 
                             <p class="white">We look forward to receiving your responses.</p>
@@ -50,47 +56,58 @@
                                 </a>
                             </div>
                             <h5 class="header-title mb-3 mob-view" style="position: relative; top: 3rem;">Register</h5>
-                            <form class="tooltip-right-bottom mob-view" style="position: relative; top: 4rem;" novalidate method="POST" action="{{ route('register', 'Alumnus') }}">
+                            <form class="tooltip-right-bottom mob-view" style="position: relative; top: 4rem;" novalidate
+                                method="POST" action="{{ route('register', 'Alumnus') }}">
                                 @csrf
 
-                                <div class="form-group has-float-label " style="margin-bottom: 2rem!important"><input value="{{old('firstName')}}" class="@error('firstName') border-danger @enderror form-control" name="firstName" required>
+                                <div class="form-group has-float-label " style="margin-bottom: 2rem!important"><input
+                                        value="{{ old('firstName') }}"
+                                        class="@error('firstName') border-danger @enderror form-control" name="firstName"
+                                        required>
                                     <span>First Name</span>
                                     @error('firstName')
-                                        <div class="invalid-tooltip d-block">{{$message}}</div>
+                                        <div class="invalid-tooltip d-block">{{ $message }}</div>
                                     @enderror
                                 </div>
-                                <div class="form-group has-float-label"><input value="{{old('lastName')}}" class="@error('lastName') border-danger @enderror form-control" name="lastName" required>
+                                <div class="form-group has-float-label"><input value="{{ old('lastName') }}"
+                                        class="@error('lastName') border-danger @enderror form-control" name="lastName"
+                                        required>
                                     <span>Last Name</span>
                                     @error('lastName')
-                                        <div class="invalid-tooltip d-block">{{$message}}</div>
+                                        <div class="invalid-tooltip d-block">{{ $message }}</div>
                                     @enderror
                                 </div>
-                                <div class="form-group has-float-label"><input value="{{old('otherName')}}" class="form-control" name="otherName" required>
+                                <div class="form-group has-float-label"><input value="{{ old('otherName') }}"
+                                        class="form-control" name="otherName" required>
                                     <span>Other Name (optional)</span>
                                     {{-- <small class="form-text text-muted">optional</small> --}}
                                 </div>
-                                <div class="form-group has-float-label"><input value="{{old('email')}}" class="@error('email') border-danger @enderror form-control" name="email" required>
+                                <div class="form-group has-float-label"><input value="{{ old('email') }}"
+                                        class="@error('email') border-danger @enderror form-control" name="email"
+                                        required>
                                     <span>E-mail</span>
                                     @error('email')
-                                        <div class="invalid-tooltip d-block">{{$message}}</div>
+                                        <div class="invalid-tooltip d-block">{{ $message }}</div>
                                     @enderror
                                 </div>
 
-                                <div class="form-group has-float-label"><input name="password" class="@error('password') border-danger @enderror form-control"
-                                        type="password" required>
+                                <div class="form-group has-float-label"><input name="password"
+                                        class="@error('password') border-danger @enderror form-control" type="password"
+                                        required>
                                     <span>Password</span>
                                     @error('password')
-                                        <div class="invalid-tooltip d-block">{{$message}}</div>
+                                        <div class="invalid-tooltip d-block">{{ $message }}</div>
                                     @enderror
                                 </div>
-                                <div class="form-group has-float-label"><input name="password_confirmation" class="form-control"
-                                        type="password" required>
+                                <div class="form-group has-float-label"><input name="password_confirmation"
+                                        class="form-control" type="password" required>
                                     <span>Confirm Password</span>
                                 </div>
-                                
-                                <button type="submit" data-toggle="modal" data-target="#checkEmailModal" class="btn btn-primary float-right">Register</button>
+
+                                <button type="submit" data-toggle="modal" data-target="#checkEmailModal"
+                                    class="btn btn-primary float-right">Register</button>
                             </form>
-                            <form action="{{route('login')}}" method="POST" id="login-admin-form">
+                            <form action="{{ route('login') }}" method="POST" id="login-admin-form">
                                 @csrf
                                 @method('put')
                             </form>
@@ -98,7 +115,7 @@
                     </div>
                 </div>
             </div>
-           
+
         </div>
     </main>
 @endsection
@@ -112,13 +129,11 @@
     <script src="{{ asset('js/scripts.js') }}"></script>
 
     <script>
-        $(function () {
+        $(function() {
             console.log('Hi');
-            $('#login-admin-btn').click(function () {
+            $('#login-admin-btn').click(function() {
                 $('#login-admin-form').submit()
             })
         })
     </script>
 @endsection
-
-
