52518846460e63c3be0cf47d3aaf58dfce01c40b
josh-boat365 <nyarko.boateng20@outlook.com>
2022-09-09 14:55:44 +0000

    Update 2022_09_09_144048_update_question_column_on_questions_table.php

--- database/migrations/2022_09_09_144048_update_question_column_on_questions_table.php
+++ database/migrations/2022_09_09_144048_update_question_column_on_questions_table.php
@@ -14,7 +14,7 @@ class UpdateQuestionColumnOnQuestionsTable extends Migration
     public function up()
     {
         Schema::table('questions', function (Blueprint $table) {
-            $table->longText('question');
+            $table->longText('question')->change();
         });
     }
 
