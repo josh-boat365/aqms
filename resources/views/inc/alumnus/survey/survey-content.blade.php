af3f9d87f18fe49f456dbfa70c38db5c0e15f03f
josh-boat365 <nyarko.boateng20@outlook.com>
2022-09-19 12:09:07 +0000

    fixed ui of survey tiles on alumnus

--- resources/views/inc/alumnus/survey/survey-content.blade.php
+++ resources/views/inc/alumnus/survey/survey-content.blade.php
@@ -1,8 +1,18 @@
 <style>
-    .tile-header h5{
-        width: 142%;
+    .tile-header h5 {
+        width: 163%;
         padding: 0;
         margin: 0;
+    }
+
+    @media (min-width: 300px) and (max-width: 900px) {
+
+        .h5 {
+            font-size: 11px !important;
+            padding: 3px;
+        }
+
+
     }
 </style>
 <main>
@@ -12,15 +22,14 @@
                 <div class="mb-2">
                     <h1>Surveys</h1>
                 </div>
-               
+
                 <div class="separator mb-5"></div>
                 {{-- @if (($updateProgress != 4 && auth()->user()->isAdmin) || ($updateProgress != 7 && !auth()->user()->isAdmin)) --}}
-                @if ($updateProgress != 7 )
-                    
-                @include('welcome')
+                @if ($updateProgress != 7)
+                    @include('welcome')
                 @endif
-                  {{-- Survey Tile Header  --}}
-                  <div class="card mb-5 tile-header">
+                {{-- Survey Tile Header --}}
+                <div class="card mb-5 tile-header">
                     <div class=" card-body row">
                         <div class="col-3">
                             <h5 class="font-weight-bold h5">Title</h5>
@@ -42,7 +51,7 @@
                 </div>
                 <div class="list disable-text-selection" data-check-all="checkAll">
                     @yield('survey-tiles')
-                   
+
                 </div>
             </div>
         </div>
@@ -54,11 +63,11 @@
                 <ul class="list-unstyled mb-5">
                     @yield('stat')
                 </ul>
-               
+
             </div>
         </div><a class="app-menu-button d-inline-block d-xl-none" href="#"><i class="simple-icon-options"></i></a>
     </div>
-    
+
 </main>
 
 @include('footer')
--- resources/views/inc/alumnus/survey/survey-tile.blade.php
+++ resources/views/inc/alumnus/survey/survey-tile.blade.php
@@ -8,36 +8,43 @@
         margin: auto;
     }
 
-    @media (min-width: 320px) and (max-width: 900px) {
+    @media (min-width: 300px) and (max-width: 900px) {
         .dash-survey-tile {
             display: flex !important;
             flex-direction: row !important;
         }
-        .tile-text{
+
+        .tile-text {
             font-size: 80%;
         }
-        .surv-name{
+
+        .surv-name {
             white-space: normal !important;
             position: relative;
             left: -1rem;
         }
-        .surv-stats{
+
+        .surv-stats {
             padding: 5px;
             font-size: 8px;
 
         }
-        
+
         .tile-info p {
             width: 4rem !important;
-            
+
         }
 
         .tile-header .card-body .col-3 h5 {
             font-size: 11px !important;
         }
 
-    }
+        .badge.badge-pill {
+            padding-left: 3px !important;
+            width: 62px !important;
+        }
 
+    }
 </style>
 @if ($submissions->where('user_id', auth()->user()->id)->where('survey_id', $survey->id)->count() == 0)
     <a href="{{ url('/home/surveys/' . $survey->id) }}">
@@ -45,7 +52,8 @@
 
 <div class="card d-flex flex-row mb-3">
     <div class="d-flex flex-grow-1 min-width-zero">
-        <div class="dash-survey-tile card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
+        <div
+            class="dash-survey-tile card-body align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
             <div class="col-3 tile-info">
                 <span class="list-item-heading mb-0 truncate w-40 w-xs-100 mt-0">
 
@@ -73,19 +81,16 @@
             <div class="col-3 tile-info">
                 <div class="w-8 w-xs-100">
                     <p class="mb-0 tile-x tile-text text-muted font-weight-bold text-medium">
-                        {{ $survey->expiration_date}}</p> 
+                        {{ $survey->expiration_date }}</p>
                 </div>
             </div>
 
             <div class="col-3 tile-info">
                 <div class="w-8 w-xs-100">
-                    <p
-                        class="mb-0 font-weight-bold surv-stats  text-medium badge badge-pill  @if ($submissions->where('user_id', auth()->user()->id)->where('survey_id', $survey->id)->count() > 0)
-                            badge-success">
+                    <p class="mb-0 font-weight-bold surv-stats  text-medium badge badge-pill  @if ($submissions->where('user_id', auth()->user()->id)->where('survey_id', $survey->id)->count() > 0) badge-success">
                         submitted
                     @else
-                        badge-primary"> not Submitted
-                        @endif
+                        badge-primary"> not Submitted @endif
                     </p>
                 </div>
             </div>
@@ -93,6 +98,6 @@
         </div>
     </div>
 </div>
-@if ($submissions->where('user_id', auth()->user()->id)->where('survey_id', $survey->id)->count() == 0)
-    </a>
-@endif
+   @if ($submissions->where('user_id', auth()->user()->id)->where('survey_id', $survey->id)->count() == 0)
+                        </a>
+                        @endif
