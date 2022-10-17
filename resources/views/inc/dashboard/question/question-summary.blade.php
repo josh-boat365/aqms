887567a70c8e52c11cff4932d19b99261c6de9dd
Fuad2Dev <94391454+Fuad2Dev@users.noreply.github.com>
2022-10-17 13:40:13 +0000

    Fix: control btn responsiveness

--- resources/views/inc/dashboard/question/question-summary.blade.php
+++ resources/views/inc/dashboard/question/question-summary.blade.php
@@ -111,9 +111,9 @@
                 <label for="" class="form-group has-float-label"
                     style="border: #c8d2dc solid 1px;border-radius: 20px; max-width:100%">
                     <div style="padding: 1rem">
-                        <button class="btn btn-outline-secondary" style="width: 10rem">Draft</button><br>
-                        <button class="btn btn-outline-success" style="width: 10rem">Deploy</button><br>
-                        <button class="btn btn-outline-danger" style="width: 10rem">Archive</button>
+                        <button class="btn btn-outline-secondary" style="width: 100%">Draft</button><br>
+                        <button class="btn btn-outline-success" style="width: 100%">Deploy</button><br>
+                        <button class="btn btn-outline-danger" style="width: 100%">Archive</button>
                     </div>
                     <span>Set Survey Status </span>
                 </label>
